fad8f8cc928594d0ee7e229df1fcb4a322aa582f
uselibrary <mail@uselib.com>
2017-12-21 17:09:59 +0800

    Add update.sh
    Add update.sh

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear
+echo "######################################"
+echo "#                                    #"
+echo "#  A update script for new Linux OS  #"
+echo "#           https://pa.ci            #"
+echo "#                                    #"
+echo "######################################"
+echo ""
+echo -e "Are you want to update and install some softwares? "
+echo -e ""
+read -p "Y or N "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e ""
+    apt-get update -y
+    apt-get install vim -y
+    apt-get install curl -y
+    apt-get install python -y
+    apt-get install python3 -y
+    apt-get upgrade -y
+    echo -e ""
+    echo -e "Update finished"
+fi
+echo ""
+echo -e "Are you want to reboot it? "
+echo -e ""
+read -p "Y or N "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    reboot
+fi
+echo -e ""
+clear
