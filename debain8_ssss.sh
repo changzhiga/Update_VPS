270ce3e393ac521f817fec17a99ec9bcc09c1999
uselibrary <mail@uselib.com>
2017-12-22 21:33:20 +0800

    Improve memory performance
    Improve memory performance

--- debain8_ssss.sh
+++ debain8_ssss.sh
@@ -27,22 +27,30 @@ then
     echo -e ""
     echo -e "Update finished"
 fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
 echo -e ""
 read  -n 1 -p "Do you want to install server speeder? [Y/n] "
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh && chmod +x appex.sh && bash appex.sh install
 fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
 echo -e ""
 read  -n 1 -p "Do you want to install shadowsocks? [Y/n] "
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh && chmod +x shadowsocks-all.sh && ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
 fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
 echo -e ""
 read  -n 1 -p "Do you want to reboot it? [Y/n] "
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     reboot
 fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
 echo -e ""
