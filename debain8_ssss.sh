144242fd4724c06d3a65df6f84980dfa9cd237cb
uselibrary <mail@uselib.com>
2017-12-22 20:59:25 +0800

    Add SSSS
    Add SSSS

---
+++ debain8_ssss.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+clear
+echo "######################################"
+echo "#                                    #"
+echo "#  A update script for new Linux OS  #"
+echo "#           https://pa.ci            #"
+echo "#      Only for Debian8 64bits       #"
+echo "#                                    #"
+echo "######################################"
+echo ""
+read  -n 1 -p "Do you want to update it? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e ""
+    apt-get update -y
+    apt-get install vim -y
+    apt-get install wget -y
+    apt-get install curl -y
+    apt-get install zip unzip -y
+    apt-get install python -y
+    apt-get install python3 -y
+    apt-get install traceroute -y
+    apt-get install net-tools -y
+    apt-get install cron -y
+    apt-get install git
+    apt-get upgrade -y
+    echo -e ""
+    echo -e "Update finished"
+fi
+echo -e ""
+read  -n 1 -p "Do you want to install server speeder? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh && chmod +x appex.sh && bash appex.sh install
+fi
+echo -e ""
+read  -n 1 -p "Do you want to install shadowsocks? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh && chmod +x shadowsocks-all.sh && ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+fi
+echo -e ""
+read  -n 1 -p "Do you want to reboot it? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    reboot
+fi
+echo -e ""
