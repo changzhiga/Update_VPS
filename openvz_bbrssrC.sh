1ef12bea358c0bd327ec71853fa1150d3f324804
uselibrary <mail@uselib.com>
2017-12-23 21:30:18 +0800

    Add openvz bbrssr C&D
    Add openvz bbrssr C&D

---
+++ openvz_bbrssrC.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+clear
+echo "######################################"
+echo "#                                    #"
+echo "#  A update script for new Linux OS  #"
+echo "#           https://pa.ci            #"
+echo "#    For CentOS/RedHat OpenVZ VPS    #"
+echo "#                                    #"
+echo "######################################"
+echo ""
+read  -n 1 -p "Do you want to update it? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e ""
+    yum update -y
+    yum install vim -y
+    yum install wget -y
+    yum install curl -y
+    yum install zip unzip -y
+    yum install python -y
+    yum install traceroute -y
+    yum install net-tools -y
+    yum install crontabs -y
+    echo -e ""
+    echo -e "Update finished"
+fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
+echo -e ""
+read  -n 1 -p "Do you want to install BBR? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget --no-check-certificate -O bbr.sh https://github.com/91yun/uml/raw/master/lkl/install.sh && chmod +x bbr.sh && bash bbr.sh
+    echo -e ""
+    echo -e "BBR installed"
+fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
+echo -e ""
+read  -n 1 -p "Do you want to install shadowsocks? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh && chmod +x shadowsocksR.sh && ./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+    echo -e ""
+    echo -e "Shadowsocks installed"
+fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
+echo -e ""
+echo "The system will reboot in 10 seconds."
+echo "To stop the reboot, press the CTRL+C key to cancel it."
+sleep 7
+echo "The system will reboot in 3 seconds."
+sleep 3
+echo "The system is rebooting."
+reboot
---
+++ openvz_bbrssrD.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+clear
+echo "######################################"
+echo "#                                    #"
+echo "#  A update script for new Linux OS  #"
+echo "#           https://pa.ci            #"
+echo "#    For Debian/Ubuntu OpenVZ VPS    #"
+echo "#                                    #"
+echo "######################################"
+echo ""
+read  -n 1 -p "Do you want to update it? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo -e ""
+    apt-get update -y
+    apt-get install vim -y
+    apt-get install wget -y
+    apt-get install curl -y
+    apt-get install zip unzip -y
+    apt-get install python -y
+    apt-get install python3 -y
+    apt-get install traceroute -y
+    apt-get install net-tools -y
+    apt-get install cron -y
+    apt-get install git
+    apt-get upgrade -y
+    echo -e ""
+    echo -e "Update finished"
+fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
+echo -e ""
+read  -n 1 -p "Do you want to install BBR? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget --no-check-certificate -O bbr.sh https://github.com/91yun/uml/raw/master/lkl/install.sh && chmod +x bbr.sh && bash bbr.sh
+    echo -e ""
+    echo -e "BBR installed"
+fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
+echo -e ""
+read  -n 1 -p "Do you want to install shadowsocks? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh && chmod +x shadowsocksR.sh && ./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+    echo -e ""
+    echo -e "ShadowsocksR installed"
+fi
+sync
+echo 1 > /proc/sys/vm/drop_caches
+echo -e ""
+echo "The system will reboot in 10 seconds."
+echo "To stop the reboot, press the CTRL+C key to cancel it."
+sleep 7
+echo "The system will reboot in 3 seconds."
+sleep 3
+echo "The system is rebooting."
+reboot
