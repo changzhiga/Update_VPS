4bee5b74728f3508b4dd547628951ff40622e02b
UseLib <34091189+uselibrary@users.noreply.github.com>
2018-01-17 21:52:33 +0800

    Update to V0.4
    Update to V0.4

--- update.sh
+++ update.sh
@@ -1,38 +1,4 @@
 #!/bin/bash
-
-clear
-echo "  ######################################"
-echo "  #                                    #"
-echo "  #  A update script for new Linux OS  #"
-echo "  #           https://pa.ci            #"
-echo "  #                                    #"
-echo "  ######################################"
-echo ""
-read  -n 1 -p "Do you want to update it? [Y/n] "
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo -e ""
-    apt-get update -y
-    apt-get install vim -y
-    apt-get install wget -y
-    apt-get install curl -y
-    apt-get install zip unzip -y
-    apt-get install python -y
-    apt-get install python3 -y
-    apt-get install traceroute -y
-    apt-get install net-tools -y
-    apt-get install cron -y
-    apt-get upgrade -y
-    echo -e ""
-    echo -e "Update finished"
-fi
-sync
-echo 1 > /proc/sys/vm/drop_caches
-echo -e ""
-echo "The system will reboot in 10 seconds."
-echo "To stop the reboot, press the CTRL+C key to cancel it."
-sleep 7
-echo "The system will reboot in 3 seconds."
-sleep 3
-echo "The system is rebooting."
-reboot
+wget --no-check-certificate -O update-U.sh https://raw.githubusercontent.com/uselibrary/Update_VPS/master/update-U.sh
+chmod +x update-U.sh
+bash update-U.sh
